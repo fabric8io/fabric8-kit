b13171ebb9ac8b214c93563fd557e6667efd6bde
Roland Huß <roland@ro14nd.de>
2017-05-31 17:41:24 +0200

    Set proper version and add doc module

---
+++ Jenkinsfile
@@ -0,0 +1,18 @@
+#!/usr/bin/groovy
+@Library('github.com/fabric8io/fabric8-pipeline-library@master')
+def dummy
+mavenNode {
+  dockerNode {
+    checkout scm
+    sh "git remote set-url origin git@github.com:fabric8io/fabric8-build.git"
+
+    def pipeline = load 'release.groovy'
+
+    stage 'Stage'
+    def stagedProject = pipeline.stage()
+
+    stage 'Promote'
+    pipeline.release(stagedProject)
+
+  }
+}
---
+++ doc/ci-docs.sh
@@ -0,0 +1,17 @@
+echo ============================================
+echo Deploying fabric8-maven-plugin documentation
+echo ============================================
+
+cd doc && \
+mvn -Phtml,pdf package && \
+git clone -b gh-pages https://rhuss:${GITHUB_TOKEN}@github.com/fabric8io/fabric8-build.git gh-pages && \
+git config --global user.email "travis@fabric8.io" && \
+git config --global user.name "Travis" && \
+cp -rv target/generated-docs/* gh-pages/ && \
+cd gh-pages && \
+mv index.pdf fabric8-build.pdf && \
+git add --ignore-errors * && \
+git commit -m "generated documentation" && \
+git push origin gh-pages && \
+cd .. && \
+rm -rf gh-pages target
