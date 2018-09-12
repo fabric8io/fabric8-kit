a37a9b364cde22d837e0c87f66f3cabc31f4ae44
Roland Huß <roland@ro14nd.de>
2018-09-12 13:05:51 +0200

    Update ci-docs.sh

--- doc/ci-docs.sh
+++ doc/ci-docs.sh
@@ -4,12 +4,12 @@ echo ============================================
 
 cd doc && \
 mvn -Phtml,pdf package && \
-git clone -b gh-pages https://rhuss:${GITHUB_TOKEN}@github.com/fabric8io/fabric8-build.git gh-pages && \
+git clone -b gh-pages https://rhuss:${GITHUB_TOKEN}@github.com/fabric8io/fabric8-kit.git gh-pages && \
 git config --global user.email "travis@fabric8.io" && \
 git config --global user.name "Travis" && \
 cp -rv target/generated-docs/* gh-pages/ && \
 cd gh-pages && \
-mv index.pdf fabric8-build.pdf && \
+mv index.pdf fabric8-kit.pdf && \
 git add --ignore-errors * && \
 git commit -m "generated documentation" && \
 git push origin gh-pages && \
